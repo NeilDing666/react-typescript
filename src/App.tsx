078396dedd242aa1a9fe45e55e64e8ae946fa6db
nd <neil@bonjoro.com>
2020-08-23 23:01:27 +1000

    Add Calculate BMI feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FormEvent, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ReactDOM from "react-dom";
+
+const input = {
+  color: "black",
+};
+// function CalBMI(person:client):number{
+//   person.BMI=person.weight/Math.pow(person.height, 2);
+//   return (person.BMI);
+// }
+
+const InputForm = () => {
+  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [BMI, setBMI] = useState("");
+
+  const onSubmit = (e: FormEvent) => {
+    console.log("Submit!");
+    e.preventDefault();
+  };
+
+  return (
+    <div className="input" style={input}>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="h">Height:</label>
+        <input id="h" onChange={(e) => setHeight(parseFloat(e.target.value))} />
+        <br />
+        <label htmlFor="w">Weight:</label>
+        <input id="w" onChange={(e) => setWeight(parseFloat(e.target.value))} />
+        <br />
+        <button
+          type="submit"
+          onClick={(e) => setBMI(String(weight / Math.pow(height, 2)))}
+        >
+          Submit
+        </button>
+      </form>
+      <div id="result">{BMI}</div>
+    </div>
+  );
+};
 
 function App() {
   return (
@@ -23,4 +63,4 @@ function App() {
   );
 }
 
-export default App;
+export default InputForm;
